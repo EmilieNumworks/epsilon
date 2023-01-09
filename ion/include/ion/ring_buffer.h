466bb525a048f63648abc8cfed1b0fce477bb1af
Emilie Feral <emilie.feral@numworks.com>
2023-01-09 17:11:22 +0100

    [ion] Fix RingBuffer from future

--- ion/include/ion/ring_buffer.h
+++ ion/include/ion/ring_buffer.h
@@ -10,26 +10,21 @@ namespace Ion {
 template<typename T, int N>
 class RingBuffer {
 public:
-  // N is a special value used for empty ring buffer
-  RingBuffer() : m_start(N), m_end(N) {}
+  RingBuffer() : m_start(0), m_length(0) {}
 
   void push(T element) {
-    assert(length() < N);
-    m_start = m_start % N;
-    m_stack[m_end % N] = element;
-    m_end = (m_end + 1) % N;
-  }
-
-  T * elementAtIndex(int index) {
-    assert(index >= 0 && index < length());
-    return &m_stack[(m_start + index) % N];
+    m_stack[nextElementIndex()] = element;
+    if (length() < N) {
+      m_length++;
+    } else {
+      m_start = (m_start + 1) % N;
+    }
   }
 
   T stackPop() {
     assert(length() > 0);
-    m_end = (m_end + N - 1) % N;
-    T res = m_stack[m_end];
-    resetIfEmpy();
+    m_length--;
+    T res = m_stack[nextElementIndex()];
     return res;
   }
 
@@ -37,33 +32,33 @@ public:
     assert(length() > 0);
     T res = m_stack[m_start];
     m_start = (m_start + 1) % N;
-    resetIfEmpy();
+    m_length--;
     return res;
   }
 
+  T * elementAtIndex(int index) {
+    assert(index >= 0 && index < static_cast<int>(length()));
+    return &m_stack[(m_start + index) % N];
+  }
+
   size_t length() const {
-    if (isEmpty()) {
-      return 0;
-    }
-    return m_start < m_end ? m_end - m_start : m_end + N - m_start;
+    assert(0 <= m_start && m_start < N);
+    assert(0 <= m_length && m_length <= N);
+    return m_length;
   }
 
-  bool isEmpty() const {
-    assert((m_start == N) == (m_end == N));
-    return m_start == N;
-  }
+  bool isEmpty() const { return m_length == 0; }
 
-  void reset() { m_start = N; m_end = N; }
+  void reset() { m_length = 0; }
 
 private:
-  void resetIfEmpy() {
-    if (m_start == m_end) {
-      reset();
-    }
+  int nextElementIndex() const {
+    return (m_start + length()) % N;
   }
+
   T m_stack[N];
   int m_start;
-  int m_end;
+  int m_length;
 };
 
 }
